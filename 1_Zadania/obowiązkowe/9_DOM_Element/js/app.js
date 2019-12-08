b127dc2957380b108b1fbc621577a1ad4c2f1e9c
Sztajner Sylwester <sylwester.sztajner@microsoft.wsei.edu.pl>
2019-12-08 09:24:55 +0100

    exercise 8

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -8,5 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
-
+//0
+function getDatasInfo(elements){
+    let newArraydata = []; 
+    for(let i = 0; i < elements.length; i++){
+        newArraydata.push(elements[i].dataset.color);
+    }
+    return newArraydata;
+}
+console.log(getDatasInfo(links));
+//1
+console.log(homeElement)
+console.log(typeof homeElement)
+console.log(childElements)
+console.log(typeof childElements)
 });
